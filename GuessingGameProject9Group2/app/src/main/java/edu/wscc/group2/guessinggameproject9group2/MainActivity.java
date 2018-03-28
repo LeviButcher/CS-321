cb15f0c07b190378d91aae45b99631fa7293a943
Levi Butcher <lbutche3@wvup.edu>
2018-03-27 21:28:54 -0400

    got it done boss

--- GuessingGameProject9Group2/app/src/main/java/edu/wscc/group2/guessinggameproject9group2/MainActivity.java
+++ GuessingGameProject9Group2/app/src/main/java/edu/wscc/group2/guessinggameproject9group2/MainActivity.java
@@ -5,15 +5,22 @@ import android.app.FragmentTransaction;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Point;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.RelativeLayout;
 
 import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
 
+/**
+ * Sets up the GuessingGame App
+ */
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +32,32 @@ public class MainActivity extends AppCompatActivity {
             BuildGui(false);
         }
 
-
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setUpGame();
+    }
+
     public void onConfigurationChanged(Configuration _config){
         super.onConfigurationChanged(_config);
-        checkDeminsions(_config);
+        checkDimensions(_config);
     }
-    public void checkDeminsions(Configuration _config){
+    public void checkDimensions(Configuration _config){
         if(_config.orientation == ORIENTATION_PORTRAIT){
             BuildGui(true);
         }else{
             BuildGui(false);
         }
     }
+
+    /**
+     * Configures the view passed on whether the device is in horizontal or vertical orientation
+     * if vertical then pass in true, if not pass in false
+     *
+     * @param _orientation true if vertical false if not
+     */
     public void BuildGui(Boolean _orientation){
         Point size = new Point();
         getWindowManager().getDefaultDisplay().getSize(size);
@@ -52,9 +72,9 @@ public class MainActivity extends AppCompatActivity {
             botParams = relativeLayoutParamsHeight((screenHeight - firstHeight));
             botParams.addRule(RelativeLayout.BELOW, topRl.getId());
         }else {
-            int firstWidth = (int) (screenWidth * .55);
+            int firstWidth = (int) (screenWidth * .65);
             topParams = relativeLayoutParamsWidth(firstWidth);
-            botParams = relativeLayoutParamsHeight((screenWidth - firstWidth));
+            botParams = relativeLayoutParamsWidth((screenWidth - firstWidth));
             botParams.addRule(RelativeLayout.RIGHT_OF, topRl.getId());
         }
         topRl.setLayoutParams(topParams);
@@ -69,16 +89,73 @@ public class MainActivity extends AppCompatActivity {
 
         TopFragment topFragment = new TopFragment();
         BottomFragment botFragment = new BottomFragment();
+        NumberGuesserFragment guesserFragment = new NumberGuesserFragment();
 
-        fragmentTransaction.add(topRl.getId(), topFragment);
-        fragmentTransaction.add(botRl.getId(), botFragment);
+        Log.d(TAG, "Id is" + topRl.getId());
+        fragmentTransaction.add(topRl.getId(), topFragment, "Top Fragment");
+        fragmentTransaction.add(botRl.getId(), botFragment, "Bot Fragment");
+        fragmentTransaction.add(guesserFragment, "Guess Fragment");
         fragmentTransaction.commit();
+
     }
+
     private static RelativeLayout.LayoutParams relativeLayoutParamsHeight(int height){
-        return new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, height);
+        return new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, height);
     }
+
     private static RelativeLayout.LayoutParams relativeLayoutParamsWidth(int width){
         return new RelativeLayout.LayoutParams(width, RelativeLayout.LayoutParams.WRAP_CONTENT);
     }
 
+
+    private void setUpGame()
+    {
+        Log.d(TAG, "Id of R.layout.bottomlayout" + R.layout.bottom_layout);
+        BottomFragment bottomFragment = (BottomFragment) getFragmentManager().findFragmentByTag("Bot Fragment");
+
+        bottomFragment.getUserGuessInput().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+                Log.d(TAG, editable.toString());
+                NumberGuesserFragment guesserFragment = (NumberGuesserFragment) getFragmentManager().findFragmentByTag("Guess Fragment");
+                TopFragment topFragment =(TopFragment) getFragmentManager().findFragmentByTag("Top Fragment");
+                topFragment.resetBuzzers();
+                Log.d(TAG, "Random Number is" + topFragment.getRandomNumber());
+                int numberEntered;
+                try
+                {
+                     numberEntered = Integer.parseInt(editable.toString());
+                }
+                catch(Exception e)
+                {
+                    Log.d(TAG, e.toString());
+                    return;
+                }
+
+                guesserFragment.setNumberToGuess(topFragment.getRandomNumber());
+                int result = guesserFragment.guessNumber(numberEntered);
+
+                if(result == 0){
+                    topFragment.toggleJustRight();
+                }
+                else if(result < 0){
+                    topFragment.toggleTooHigh();
+                }
+                else{
+                    topFragment.toggleTooLow();
+                }
+            }
+        });
+    }
 }
--- GuessingGameProject9Group2/app/src/main/java/edu/wscc/group2/guessinggameproject9group2/TopFragment.java
+++ GuessingGameProject9Group2/app/src/main/java/edu/wscc/group2/guessinggameproject9group2/TopFragment.java
@@ -2,12 +2,17 @@ package edu.wscc.group2.guessinggameproject9group2;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +22,40 @@ import android.widget.RelativeLayout;
 import android.widget.Space;
 import android.widget.TextView;
 
-/**
- * Created by rober on 3/26/2018.
- */
 
+/**
+ * Represents the top Fragment in the NumberGuesser App<br/>
+ *
+ * contains two fields for inserting the low and high values of random number generated<br/>
+ *
+ * and contains buzzers containing the text TooHigh, TooLow, Correct. Methods are available for setting the background
+ * for each and resetting the background back to defaults<br/>
+ */
 public class TopFragment extends Fragment {
+
+    private final String TAG = "TopFragment";
+    private final String NUMBER_GENERATOR = "Number Generator";
+
+    private TextView tooLow;
+    private TextView tooHigh;
+    private TextView justRight;
+    private int unHighlighted = Color.DKGRAY;
+    private int toggleColor = Color.GREEN;
+    private int incorrectColor = Color.parseColor("#f6b307");
+    private int correctColor = Color.parseColor("#369a2c");
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Number generator is only used by this fragment
+        FragmentManager fragmentManger = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManger.beginTransaction();
+        RandomNumberFragment rnf = new RandomNumberFragment();
+        fragmentTransaction.add(rnf, NUMBER_GENERATOR);
+        fragmentTransaction.commit();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         Point size = new Point();
@@ -47,6 +81,10 @@ public class TopFragment extends Fragment {
         minValue.setHint("Min. Value (default 1)");
         maxValue.setHint("Max Value (default 1)");
         theMainRelative.addView(editTextLl);
+        RelativeLayout.LayoutParams forBuzzers = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        forBuzzers.addRule(RelativeLayout.CENTER_VERTICAL);
+        forBuzzers.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        theMainRelative.addView(getBuzzers(), forBuzzers);
 
         minValue.addTextChangedListener(new minTextWatcher(minValue));
         maxValue.addTextChangedListener(new maxTextWatcher(maxValue));
@@ -55,6 +93,106 @@ public class TopFragment extends Fragment {
 
         return v;
     }
+
+    private View getBuzzers()
+    {
+        Activity _this = getActivity();
+        LinearLayout ll = new LinearLayout(_this);
+        LinearLayout.LayoutParams llParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        ll.setLayoutParams(llParams);
+        llParams.setMargins(40,40,40,40);
+
+
+        LinearLayout.LayoutParams spacerParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, .05f);
+        Space spacer1 = new Space(_this);
+        Space spacer2 = new Space(_this);
+        spacer1.setLayoutParams(spacerParams);
+        spacer2.setLayoutParams(spacerParams);
+
+        tooLow = new TextView(_this);
+        tooHigh = new TextView(_this);
+        justRight = new TextView(_this);
+
+        ll.addView(tooLow);
+        ll.addView(spacer1);
+        ll.addView(justRight);
+        ll.addView(spacer2);
+        ll.addView(tooHigh);
+
+
+        tooLow.setText("Too Low");
+
+        tooHigh.setText("Too High");
+
+        justRight.setText("Correct");
+
+        defaultBuzzerStyle(tooHigh);
+        defaultBuzzerStyle(tooLow);
+        defaultBuzzerStyle(justRight);
+
+        return ll;
+    }
+
+    /**
+     * Change the TooLow TextView to have it's toggle Color
+     *
+     * Precondition: View is rendered
+     * Postcondition: TooLow EditText has a different Background Color
+     */
+    public void toggleTooLow(){
+        tooLow.setTextColor(Color.parseColor("#FFFFFF"));
+        tooLow.setBackgroundColor(incorrectColor);
+    }
+
+
+    /**
+     * Change the TooHigh TextView to have it's toggle Color
+     *
+     * Precondition: View is rendered
+     * Postcondition: EditText has a different Background Color
+     */
+    public void toggleTooHigh(){
+        tooHigh.setTextColor(Color.parseColor("#FFFFFF"));
+        tooHigh.setBackgroundColor(incorrectColor);
+    }
+
+    /**
+     * Change the Correct TextView to have it's toggle Color
+     *
+     * Precondition: View is rendered
+     * Postcondition: EditText has a different Background Color
+     */
+    public void toggleJustRight(){
+        justRight.setBackgroundColor(correctColor);
+        justRight.setTextColor(Color.parseColor("#FFFFFF"));
+    }
+
+    /**
+     * Reset all the buzzers to the there original background color
+     *
+     * Precondition: View is rendered
+     * Postcondition: All Buzzers are back to there original background
+     */
+    public void resetBuzzers(){
+        tooLow.setBackgroundColor(unHighlighted);
+        tooHigh.setBackgroundColor(unHighlighted);
+        justRight.setBackgroundColor(unHighlighted);
+    }
+
+    public static void defaultBuzzerStyle(TextView _inTextView){
+        View theParent = (View)_inTextView.getParent();
+        _inTextView.setBackgroundColor(Color.parseColor("#d8b7ca"));
+        _inTextView.setTextColor(Color.parseColor("#d7d7ed"));
+        _inTextView.setTextSize(20);
+        _inTextView.setPadding(30, 10,30,10);
+        _inTextView.setGravity(Gravity.CENTER);
+        _inTextView.setAllCaps(true);
+
+        _inTextView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, .30f));
+    }
+
+
+
     public static void editTextStyle(EditText _theField){
         View theParent = (View)_theField.getParent();
         _theField.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT, .40f));
@@ -64,43 +202,88 @@ public class TopFragment extends Fragment {
     }
 
 
+    /**
+     * Retrieves the random number that has been generated
+     *
+     * @return Random Number
+     */
+    public int getRandomNumber(){
+        RandomNumberFragment ngf = (RandomNumberFragment) getFragmentManager().findFragmentByTag(NUMBER_GENERATOR);
+        return ngf.getGeneratedNumber();
+    }
+
 
 
     private class minTextWatcher implements TextWatcher {
-        private int minValue;
+        private int minValue = 1;
         private EditText ChangedText;
+
         public minTextWatcher(EditText inEditText){
-            try{
+            try
+            {
                 String theInput = inEditText.toString();
                 minValue = Integer.parseInt(theInput);
-            }catch(NumberFormatException nfe){
+            }
+            catch(NumberFormatException nfe){
                 System.out.println(nfe);
             }
         }
         @Override
         public void afterTextChanged(Editable e){
             // the Class to update the minimum value;
+            RandomNumberFragment ngf = (RandomNumberFragment) getFragmentManager().findFragmentByTag(NUMBER_GENERATOR);
+            try{
+                minValue = Integer.parseInt(e.toString());
+                ngf.setLow(minValue);
+                Log.d(TAG, "Min Value is" + minValue);
+                ngf.generateRandomNumber();
+            }
+            catch(NumberFormatException nfe)
+            {
+                Log.d(TAG, nfe.toString());
+            }
         }
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after){};
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {}
     }
+
+
     private class maxTextWatcher implements TextWatcher {
-        private int maxValue;
+        private int maxValue = 1;
         private EditText ChangedText;
+
         public maxTextWatcher(EditText inEditText){
-            try{
+            try
+            {
+                //Constructor is only ever called apon instantiation, really no point to it as far as I can tell
                 String theInput = inEditText.toString();
                 maxValue = Integer.parseInt(theInput);
-            }catch(NumberFormatException nfe){
+                Log.d(TAG, "Construct maxValue is " + maxValue);
+            }
+            catch(NumberFormatException nfe){
                 System.out.println(nfe);
+                Log.d(TAG, nfe.toString());
             }
         }
         @Override
         public void afterTextChanged(Editable e){
             // Update the Maximum value;
+            RandomNumberFragment ngf = (RandomNumberFragment) getFragmentManager().findFragmentByTag(NUMBER_GENERATOR);
+            Log.d(TAG, "Editable is" + e.toString());
+            try{
+                maxValue = Integer.parseInt(e.toString());
+                ngf.setHigh(maxValue);
+                Log.d(TAG, "Max Value is" + maxValue);
+                ngf.generateRandomNumber();
+            }
+            catch(NumberFormatException nfe)
+            {
+                Log.d(TAG, nfe.toString());
+            }
         }
+
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after){};
         @Override
